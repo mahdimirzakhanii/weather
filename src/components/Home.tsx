bfad1d9f55d9ebf202e1e1a6732f0822015cc5a5
mahdimirzakhani4 <mahdimirzakhani85@gmail.com>
2025-06-14 23:03:45 +0330

    add location user

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -24,15 +24,44 @@ interface IWeatherItems {
   main?: "Clouds";
 }
 
+interface Location {
+  latitude: number;
+  longitude: number;
+}
+
 const Home = () => {
   const [fullData, setFullData] = useState<TData | null>(null);
   const [search, setSearch] = useState<string>("");
 
+  const [location, setLocation] = useState<Location | null>(null);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        (err) => {
+          setError("اجازه دسترسی به موقعیت داده نشد یا خطا رخ داد.");
+          console.error(err);
+        }
+      );
+    } else {
+      setError("مرورگر شما از Geolocation پشتیبانی نمی‌کند.");
+    }
+  }, []);
+
   useEffect(() => {
     const getData = async () => {
+      if (!location) return;
       try {
         const res = await axios.get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=b0929da981a188d7739b38288dbfe378&units=metric`
+          search
+            ? `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=b0929da981a188d7739b38288dbfe378&units=metric`
+            : `https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=b0929da981a188d7739b38288dbfe378&units=metric`
         );
         console.log(res?.data);
         setFullData(res?.data);
@@ -40,8 +69,10 @@ const Home = () => {
         console.log(error);
       }
     };
+
     getData();
-  }, [search]);
+  }, [location, search]);
+
   return (
     <div
       className={`text-white bg-[url(/rainy-2.jpg)] bg-cover min-h-screen h-screen flex items-center justify-around`}
